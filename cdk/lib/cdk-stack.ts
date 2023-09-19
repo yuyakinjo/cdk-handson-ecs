4df0a5364b05521d01b0913d48cacd7646ac9656
yuya <kinjo.bobchin@gmail.com>
2023-09-19 11:08:57 +0900

    feat: rds 追加

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,7 +1,8 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Vpc } from 'aws-cdk-lib/aws-ec2';
-import { CpuArchitecture, EcrImage, OperatingSystemFamily } from 'aws-cdk-lib/aws-ecs'; // prettier-ignore
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { InstanceClass, InstanceSize, InstanceType, Port, SubnetType, Vpc, } from "aws-cdk-lib/aws-ec2"; // prettier-ignore
+import { CpuArchitecture, EcrImage, OperatingSystemFamily, } from "aws-cdk-lib/aws-ecs"; // prettier-ignore
 import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
+import { AuroraPostgresEngineVersion, ClusterInstance, DatabaseCluster, DatabaseClusterEngine, InstanceUpdateBehaviour, ServerlessCluster, } from "aws-cdk-lib/aws-rds"; // prettier-ignore
 import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
 import { join } from 'path';
@@ -21,7 +22,28 @@ export class CdkStack extends Stack {
     const vpcId = StringParameter.valueFromLookup(this, vpcIdParameterName); // ハンズオンのため、VpcStackがないためのワークアラウンド
     const vpc = Vpc.fromLookup(this, Vpc.name, { vpcId }); // vpc上限にひっかかるため、ひとつのvpcを使いまわす
 
-    const { targetGroup } = new ApplicationLoadBalancedFargateService(
+    const rds = new DatabaseCluster(this, ServerlessCluster.name, {
+      vpc,
+      vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
+      engine: DatabaseClusterEngine.auroraPostgres({
+        version: AuroraPostgresEngineVersion.VER_15_3,
+      }),
+      instanceUpdateBehaviour: InstanceUpdateBehaviour.ROLLING,
+      writer: ClusterInstance.provisioned('WriterInstanceT3Medium', {
+        instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MEDIUM),
+        enablePerformanceInsights: true,
+      }),
+      readers: [
+        ClusterInstance.provisioned('ReaderInstanceT3Medium', {
+          instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MEDIUM),
+          enablePerformanceInsights: true,
+        }),
+      ],
+      defaultDatabaseName: 'zon100',
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    const { targetGroup, service } = new ApplicationLoadBalancedFargateService(
       this,
       ApplicationLoadBalancedFargateService.name,
       {
@@ -29,7 +51,14 @@ export class CdkStack extends Stack {
         taskImageOptions: {
           image,
           containerPort: 3000,
-          command: ['bun', '--watch', 'src/index.ts'],
+          command: ['bun', '--watch', './src/index.ts'],
+          environment: {
+            DB_HOST: rds.secret?.secretValueFromJson('host').unsafeUnwrap() ?? '',
+            DB_PORT: rds.secret?.secretValueFromJson('port').unsafeUnwrap() ?? '',
+            DB_USER: rds.secret?.secretValueFromJson('username').unsafeUnwrap() ?? '',
+            DB_PASS: rds.secret?.secretValueFromJson('password').unsafeUnwrap() ?? '',
+            DB_NAME: rds.secret?.secretValueFromJson('dbname').unsafeUnwrap() ?? '',
+          },
         },
         circuitBreaker: { rollback: true },
         enableExecuteCommand: true,
@@ -45,5 +74,9 @@ export class CdkStack extends Stack {
       port: '3000',
       path: '/',
     });
+
+    rds.connections.allowFrom(service, Port.tcp(rds.clusterEndpoint.port));
+
+    rds.secret?.grantRead(service.taskDefinition.taskRole);
   }
 }
